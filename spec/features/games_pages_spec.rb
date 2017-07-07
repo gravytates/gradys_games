5b65555e8f864ea3e26e25dffa7cdb26b7faf69c
Grady L Shelton <gradyish@gmail.com>
2017-07-07 14:11:45 -0700

    pass specs for filtering games

---
+++ spec/features/games_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "admin user routes and features" do
+  it 'will test filtering on games page' do
+    user = FactoryGirl.create(:admin_user)
+    login_as(user, :scope => :user)
+    FactoryGirl.create(:game)
+    visit games_path
+    click_link 'PS4'
+    expect(page).to have_content 'Final Fantasy'
+    click_link 'Switch'
+    expect(page).to have_content 'No results returned. Choose another filter!'
+  end
+end

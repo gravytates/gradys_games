188d1058e6b112401732ac76ced8d36a9f23c052
Grady L Shelton <gradyish@gmail.com>
2017-07-07 11:27:13 -0700

    pass integration spec for user login signup and signout

---
+++ spec/features/user_login_pages_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "sign up, sign out and sign in" do
+  it "will sign a user up" do
+    visit root_path
+    click_link "Sign Up"
+    fill_in "user_name", with: "christ"
+    fill_in "user_email", with: "christ@god.com"
+    fill_in "user_password", with: "123456"
+    fill_in "user_password_confirmation", with: "123456"
+    click_button "Sign up"
+    expect(page).to have_content "You have signed up successfully."
+  end
+
+  it 'will sign in a user' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+  end
+
+  it 'will sign out a user' do
+    user = FactoryGirl.create(:admin_user)
+    login_as(user, :scope => :user)
+    logout(:admin_user)
+    login_as(user, :scope => :user)
+  end
+
+
+end

190e627ad8c41f61be7ffa2910f97b7bef6a8a6a
Grady L Shelton <gradyish@gmail.com>
2017-07-07 11:39:12 -0700

    pass integration test for admin ability to add game

---
+++ spec/features/admin_function_pages_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "admin user routes and features" do
+  it 'will create a new game' do
+    user = FactoryGirl.create(:admin_user)
+    login_as(user, :scope => :user)
+    visit root_path
+    click_link 'Games'
+    expect(page).to have_content 'All Games'
+    click_link 'Add New Game'
+    fill_in 'game_name', with: 'Mario'
+    fill_in 'game_description', with: 'Italian Plumber saves the world'
+    fill_in 'game_price', with: 59.99
+    select "Platformer", :from => "game_genre"
+    select "Switch", :from => "game_platform"
+    fill_in 'game_image', with: 'http://png-4.findicons.com/files/icons/2297/super_mario/256/paper_mario.png'
+    click_button 'Create Game'
+    expect(page).to have_content 'Game successfully added!'
+  end
+end
